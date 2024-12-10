d256effeab51325a557c815f060e566b27065e1a
Kyle Engreso <202280348@psu.palawan.edu.ph>
2024-12-10 13:46:54 +0800

    feat(tables): add API endpoint to read vehicles, locations, rentals

--- app.py
+++ app.py
@@ -37,11 +37,84 @@ def get_customers():
         return jsonify({"error": "No customers found"}), 404
 
     customers_list = [
-        {"customer_id": customer[0], "customer_name": customer[1], "customer_contact": customer[2]}
+        {
+        "customer_id": customer[0], 
+        "customer_name": customer[1], 
+        "customer_contact": customer[2]
+        }
         for customer in customers
     ]
 
     return jsonify(customers_list), 200
+
+#READ VEHICLES
+@app.route("/vehicles", methods=["GET"])
+def get_vehicles():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM Vehicles")
+    vehicles = cursor.fetchall()
+
+    if not vehicles:
+        return jsonify({"error": "No vehicles found"}), 404
+
+    vehicles_list = [
+        {
+            "vehicle_id": vehicle[0],
+            "reg_number": vehicle[1],
+            "model_name": vehicle[2],
+            "daily_hire_rate": vehicle[3],
+            "vehicle_type": vehicle[4]
+        }
+        for vehicle in vehicles
+    ]
+    
+    return jsonify(vehicles_list), 200
+
+#READ LOCATIONS
+@app.route("/locations", methods=["GET"])
+def get_locations():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM Locations")
+    locations = cursor.fetchall()
+
+    if not locations:
+        return jsonify({"error": "No locations found"}), 404
+
+    locations_list = [
+        {
+            "location_id": location[0],
+            "location_name": location[1],
+            "vehicle_id": location[2],
+            "is_available": location[3]
+        }
+        for location in locations
+    ]
+    
+    return jsonify(locations_list), 200
+
+#READ RENTAL
+@app.route("/rentals", methods=["GET"])
+def get_rentals():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM Rentals")
+    rentals = cursor.fetchall()
+
+    if not rentals:
+        return jsonify({"error": "No rentals found"}), 404
+
+    rentals_list = [
+        {
+            "rental_id": rental[0],
+            "customer_id": rental[1],
+            "vehicle_id": rental[2],
+            "date_from": rental[3],
+            "date_to": rental[4],
+            "total_cost": rental[5]
+        }
+        for rental in rentals
+    ]
+    
+    return jsonify(rentals_list), 200
 
 # ADD CUSTOMERS
 @app.route("/customers", methods=["POST"])
