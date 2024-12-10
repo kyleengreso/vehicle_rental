51697966546d2d7cecc4c1c4432fab8ef4539861
Kyle Engreso <202280348@psu.palawan.edu.ph>
2024-12-10 12:50:19 +0800

    feat(web): add homepage

--- app.py
+++ app.py
@@ -11,5 +11,20 @@ app.config["MYSQL_DB"] = "vehicle_rental_db"
 
 mysql = MySQL(app)
 
+@app.route("/")
+def hello_world():
+    
+    style = """
+        <style>
+            p {
+                font-family: Arial, sans-serif;
+                font-size: 50px;
+                color: blue;
+                text-align: center;
+            }
+        </style>
+    """
+    return f"{style} <p>VEHICLE RENTAL SYSTEM MANAGEMENT</p>"
+
 if __name__ == "__main__":
     app.run(debug=True)
